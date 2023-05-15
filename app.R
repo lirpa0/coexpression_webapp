b4e827b93bdc0e142c646fc430cbc1681c9a2a42
Omer Acar <omeracar0@gmail.com>
2023-05-15 15:24:03 -0400

    clarified ui script with sidepanels and dropdown menus

--- app.R
+++ app.R
@@ -1,27 +1,12 @@
 library(shiny)
 library(dplyr)
+library(magrittr)
 library(DT)
 library(shinydashboard)
 library(shinyjs)
 
 source("ui.R")
-orf_info<- data.frame('transcript'=c("chr1_43730", "chr1_200860",'chr1_180543','chr1_200946','chr1_67378'),
-                      classification = c("canonical", "noncanonical",'noncanonical','canonical','canonical'))
-
-rho_percentile <- readRDS('/home/aar75/coexpression/20221110_rho/spqn_raw5_sample400.RDS')[1:5,1:5]
-rho_percentile<-as.data.frame(rho_percentile)
-
-gsea_df <- data.frame(transcript = c("chr1_67378", "chr1_67378",'chr1_180543'),
-                      gene = c("TFC3", "TFC3",NA),
-                      pathway_id = c("GO:0006355", "GO:0006996","GO:0006996"),
-                      pathway_name = c("Regulation of transcription, DNA-templated", "Organelle organization","Organelle organization"),
-                      padj = c(0.038, 0.017,0.0001),
-                      NES = c(1.23, 2.56,4.5),
-                      size = c(115, 240, 240))
-sequence_info_df <- data.frame(transcript = c("chr1_43730 ",'chr1_67378'),
-                               gene = c(NA,'gene1'),
-                               cds_seq = c("ATG...", "ATG..."),
-                               aa_seq = c("M...", "M..."))
+source("server.R")
 
 
 
--- ui.R
+++ ui.R
@@ -1,45 +1,40 @@
+
 ui <- fluidPage(
   
   tags$head(
     tags$link(rel = 'stylesheet', type="text/css", href = "styles.css")
   ),
   titlePanel("ORF Information App"),
-  conditionalPanel(
-    condition = "input.view == 'main'",
-    fluidRow(
-      column(6, actionButton("seq_info", "Get Sequence Information", width = "100%", style = "font-size: 24px;")),
-      column(6, actionButton("coexp_info", "Get Coexpression Information", width = "100%", style = "font-size: 24px;"))
-    ),
-    br(),
+  sidebarLayout(
+    sidebarPanel(
     textInput("orf_name", "Enter your ORF name here", width = "50%"),
-    actionButton("submit_orf", "Submit")
-  ),
-  conditionalPanel(
-    condition = "input.view == 'seq_info_page'",
-    h2("Sequence Information for ORF ", tags$span(textOutput("orf_name_seq"), style = "font-weight: bold;")),
-    h3("CDS Sequence"),
-    verbatimTextOutput("cds_sequence"),
-    h3("Amino Acid Sequence"),
-    verbatimTextOutput("aa_sequence"),
-    actionButton("return_main", "Return to Main Page", style = "position: absolute; top: 0; right: 0;")
-  ),
-  conditionalPanel(
-    condition = "input.view == 'coexp_info_page'",
-    h2("Coexpression Info for ORF ", tags$span(textOutput("orf_name_coexp"), style = "font-weight: bold;")),
-    fluidRow(
-      column(6,
-             h3("Coexpression"),
-             selectInput("orf_class_filter", "Filter by ORF classification:", c("all", "canonical", "noncanonical")),
-             DT::dataTableOutput("coexp_table")
-      ),
-      column(6,
-             h3("Gene Set Enrichment Analysis"),
-             DT::dataTableOutput("gsea_table")
-      )
+    selectInput("view", "Result Type",
+                c(Sequence = "seq_info_page", Coexpression = "coexp_info_page")
     ),
-    actionButton("return_main2", "Return to Main Page", style = "position: absolute; top: 0; right: 0;")
-  ),
-  hidden(
-    radioButtons("view", "", choices = c("main", "seq_info_page", "coexp_info_page"), selected = "main", inline = TRUE)
+    actionButton("submit_orf", "Submit")),
+  mainPanel(
+    conditionalPanel(condition = 'input.submit_orf',
+    conditionalPanel(
+      condition = "input.view == 'seq_info_page'",
+      h2("Sequence Information for ORF ", tags$span(textOutput("orf_name_seq"), style = "font-weight: bold;")),
+      h3("CDS Sequence"),
+      verbatimTextOutput("cds_sequence"),
+      h3("Amino Acid Sequence"),
+      verbatimTextOutput("aa_sequence")#,
+    ),
+    conditionalPanel(
+      condition = "input.view == 'coexp_info_page'",
+      h2("Coexpression Info for ORF ", tags$span(textOutput("orf_name_coexp"), style = "font-weight: bold;")),
+      fluidRow(
+        column(6,
+               h3("Coexpression"),
+               selectInput("orf_class_filter", "Filter by ORF classification:", c("all", "canonical", "noncanonical")),
+               DT::dataTableOutput("coexp_table")
+        ),
+        column(6,
+               h3("Gene Set Enrichment Analysis"),
+               DT::dataTableOutput("gsea_table")
+        ))
   )
 )
+)))
