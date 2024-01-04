df8c691ebed7cb9b083085198e81ab8d207ca9f3
Omer Acar <omeracar0@gmail.com>
2024-01-04 14:14:35 -0500

    update server.R from shiny folder

--- server.R
+++ server.R
@@ -8,12 +8,12 @@ library(rclipboard)
 
 
 server <- function(input, output, session) {
-
+  
   observeEvent(input$submit_orf, {
     updateSliderInput(session, "thr", value = 1)
     orf_name <- eventReactive(input$submit_orf,
                               {
-                              convertOrfName(input$orf_name)
+                                convertOrfName(input$orf_name)
                               })
     if (is.null(orf_name())) {
       showModal(modalDialog(
@@ -22,7 +22,7 @@ server <- function(input, output, session) {
         easyClose = TRUE
       ))
     } else {
-     
+      
       output$orf_name_seq <- renderText(orf_ids[[orf_name()]])
       output$orf_name_coexp<-renderText(orf_ids[[orf_name()]])
       seq_info = reactive(getSeqData(orf_name()))()
@@ -48,98 +48,98 @@ server <- function(input, output, session) {
       output$browser <- renderUI({
         tagList("", browser_link())
       })
-    coexp_data<- reactive(getCoexpData(orf_name()))
-    if(is.null(coexp_data())){
-      output$coexp_table <- NULL
-    }else{
-      gsea_data <- reactive(getGseaData(orf_name()))
-      
-      orf_class_filter <- reactive({
-        case_when(
-          input$orf_class_filter == "all" ~ 'all',
-          input$orf_class_filter == "canonical" ~ 'canonical',
-          input$orf_class_filter == "noncanonical" ~ 'noncanonical'
-        )
-      })
-      
-      output$coexp_table <- DT::renderDataTable({
-        if (orf_class_filter() == 'all') {
-          coexp_data_filtered <- coexp_data()
-        } else {
-          coexp_data_filtered <- coexp_data() %>%
-            filter(is_canonical == orf_class_filter())
-        }
+      coexp_data<- reactive(getCoexpData(orf_name()))
+      if(is.null(coexp_data())){
+        output$coexp_table <- NULL
+      }else{
+        gsea_data <- reactive(getGseaData(orf_name()))
         
-        coexp_data_show<-coexp_data_filtered%>%
-          rename("ORF ID" = "orf_id",
-                 "Systematic name"="gene",
-                 "Gene name" = "GENENAME",
-                 "ORF classification" = "is_canonical",
-                 "Coexpression (rho)" = "coexpression_percentile")%>%
-          filter(transcript!=orf_name())%>%
-          select(-transcript)
-        
-        output$downloadData <- downloadHandler(
-          filename = function() {
-            paste0(input$orf_name,"_coexpression.csv")
-          },
-          content = function(con) {
-            write.csv(coexp_data_show, con)
-          }
-        )
-        
-        DT::datatable(
-          coexp_data_show,
-          rownames = FALSE,
-          filter = "none",
-          options = list(
-            scrollX = TRUE, pageLength = 10, autoWidth = F, dom= "lfrtip"))
-        
-      })
-      output$gsea_table <- DT::renderDataTable(server=T,{
-        DT::datatable(
-          gsea_data()[, c("pathway","TERM","padj", "NES")],
-          rownames = FALSE,
-          filter = "none",
-          extensions = c('Buttons'),
-          options = list(
-            buttons=list(
-              list(extend = "csv", text = "Download GSEA Results", filename = paste0(input$orf_name,"_gsea"),
-                   exportOptions = list(
-                     modifier = list(page = "all")
-                   )
-              )
-            ),
-            scrollX = TRUE, pageLength = 10, 
-            autoWidth = F,dom= "Blfrtip"),
-          class='display'
+        orf_class_filter <- reactive({
+          case_when(
+            input$orf_class_filter == "all" ~ 'all',
+            input$orf_class_filter == "canonical" ~ 'canonical',
+            input$orf_class_filter == "noncanonical" ~ 'noncanonical'
           )
-      })
-      thr = quantile(coexp_data()$coexpression_percentile,.999)
-      updateSliderInput(session, "thr", value = as.numeric(thr))
-      
-      observeEvent(input$thr, {
-        d3_compatible_network <- reactive({
-          
-          d3_compatible_network<-getNetwork(orf_name(), coexp_data(), input$thr)
-          
         })
         
-        myColors <- paste0('d3.scaleOrdinal()
+        output$coexp_table <- DT::renderDataTable({
+          if (orf_class_filter() == 'all') {
+            coexp_data_filtered <- coexp_data()
+          } else {
+            coexp_data_filtered <- coexp_data() %>%
+              filter(is_canonical == orf_class_filter())
+          }
+          
+          coexp_data_show<-coexp_data_filtered%>%
+            rename("ORF ID" = "orf_id",
+                   "Systematic name"="gene",
+                   "Gene name" = "GENENAME",
+                   "ORF classification" = "is_canonical",
+                   "Coexpression (rho)" = "coexpression_percentile")%>%
+            filter(transcript!=orf_name())%>%
+            select(-transcript)
+          
+          output$downloadData <- downloadHandler(
+            filename = function() {
+              paste0(input$orf_name,"_coexpression.csv")
+            },
+            content = function(con) {
+              write.csv(coexp_data_show, con)
+            }
+          )
+          
+          DT::datatable(
+            coexp_data_show,
+            rownames = FALSE,
+            filter = "none",
+            options = list(
+              scrollX = TRUE, pageLength = 10, autoWidth = F, dom= "lfrtip"))
+          
+        })
+        output$gsea_table <- DT::renderDataTable(server=T,{
+          DT::datatable(
+            gsea_data()[, c("pathway","TERM","padj", "NES")],
+            rownames = FALSE,
+            filter = "none",
+            extensions = c('Buttons'),
+            options = list(
+              buttons=list(
+                list(extend = "csv", text = "Download GSEA Results", filename = paste0(input$orf_name,"_gsea"),
+                     exportOptions = list(
+                       modifier = list(page = "all")
+                     )
+                )
+              ),
+              scrollX = TRUE, pageLength = 10, 
+              autoWidth = F,dom= "Blfrtip"),
+            class='display'
+          )
+        })
+        thr = quantile(coexp_data()$coexpression_percentile,.999)
+        updateSliderInput(session, "thr", value = as.numeric(thr))
+        
+        observeEvent(input$thr, {
+          d3_compatible_network <- reactive({
+            
+            d3_compatible_network<-getNetwork(orf_name(), coexp_data(), input$thr)
+            
+          })
+          
+          myColors <- paste0('d3.scaleOrdinal()
                   .domain(["' ,  d3_compatible_network()$nodes[1,2], '", "Canonical", "Noncanonical"])
                   .range(["#fdc086",  "#7570b3", "#1b9e77"])')
-
-        output$force <- renderForceNetwork({
-          forceNetwork(Links = d3_compatible_network()$links, Nodes = d3_compatible_network()$nodes, Source = "source",
-                       Target = "target", NodeID = "name",
-                       Group = "group",zoom=TRUE, opacity=1,fontSize=15,fontFamily = "Source Sans Pro",
-                       colourScale = JS(myColors), legend=TRUE)
+          
+          output$force <- renderForceNetwork({
+            forceNetwork(Links = d3_compatible_network()$links, Nodes = d3_compatible_network()$nodes, Source = "source",
+                         Target = "target", NodeID = "name",
+                         Group = "group",zoom=TRUE, opacity=1,fontSize=15,fontFamily = "Source Sans Pro",
+                         colourScale = JS(myColors), legend=TRUE)
+          })
+          
         })
-
-      })
-     
-
-    }
+        
+        
+      }
     }
     
     output$clip_nt <- renderUI({
@@ -162,6 +162,6 @@ server <- function(input, output, session) {
         )
       })
     })
-    })
-
+  })
+  
 }
